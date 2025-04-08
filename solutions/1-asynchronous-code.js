ec77a23c9ad88efbb1b9c39532e80b22505c154c
dima55279 <dima55279@yandex.ru>
2025-04-08 13:59:46 +0300

    Выполнено задание 1.

--- solutions/1-asynchronous-code.js
+++ solutions/1-asynchronous-code.js
@@ -1,5 +1,9 @@
 import fs from 'fs';
 
 // BEGIN
-
+const print = (filepath) => {
+    const callback = (_error, data) => console.log(data);
+    fs.readFile(filepath, 'utf8', callback);
+}
+export default print;
 // END
