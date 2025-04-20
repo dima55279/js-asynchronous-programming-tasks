9cd168eb46121770d360de6f5cc0478cd4ae581d
dima55279 <dima55279@yandex.ru>
2025-04-20 14:15:52 +0300

    Выполнено задание 2.

--- solutions/2-asynchronous-return.js
+++ solutions/2-asynchronous-return.js
@@ -1,5 +1,10 @@
 import fs from 'fs';
 
 // BEGIN
-
+const write = (filepath, data, callback) => {
+    if (data === "")
+        return;
+    fs.writeFile(filepath, data, callback);
+}
+export default write;
 // END
