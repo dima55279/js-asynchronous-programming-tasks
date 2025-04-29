90c27ed06d1407cc516e9f33ef5980881df2eef6
dima55279 <dima55279@yandex.ru>
2025-04-29 09:50:15 +0300

    Выполнено задание 3.

--- solutions/3-asynchronous-flow.js
+++ solutions/3-asynchronous-flow.js
@@ -1,5 +1,14 @@
 import fs from 'fs';
 
 // BEGIN
-
+export const compareFileSizes = (firstFilepath, secondFilepath, callback) => {
+    let firstSize, secondSize;
+    fs.stat(firstFilepath, (_err, firstStats) => {
+        firstSize = firstStats.size;
+        fs.stat(secondFilepath, (_err, secondStats) => {
+            secondSize = secondStats.size;
+            callback(null, Math.sign(firstSize - secondSize));
+        });
+    });
+};
 // END
