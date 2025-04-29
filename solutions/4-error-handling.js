240173bb8626ebe8324d278886edba73bed914c7
dima55279 <dima55279@yandex.ru>
2025-04-29 10:10:14 +0300

    Выполнено задание 4.

--- solutions/4-error-handling.js
+++ solutions/4-error-handling.js
@@ -1,5 +1,25 @@
 import fs from 'fs';
 
 // BEGIN
-
+export const move = (filepathFrom, filepathTo, callback) => {
+    fs.readFile(filepathFrom, (err, data) => {
+        if (err) {
+            callback(err);
+            return;
+        }
+        fs.writeFile(filepathTo, data, (err) => {
+            if (err) {
+                callback(err);
+                return;
+            }
+            fs.unlink(filepathFrom, (err) => {
+                if (err) {
+                    callback(err);
+                    return;
+                }
+                callback(null);
+            });
+        });
+    });
+}
 // END
