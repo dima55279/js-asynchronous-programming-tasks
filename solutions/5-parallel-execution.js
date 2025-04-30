2403f747638e8522babe22e93284dc9cf805622f
dima55279 <dima55279@yandex.ru>
2025-05-01 01:59:32 +0300

    Выполнено задание 5.

--- solutions/5-parallel-execution.js
+++ solutions/5-parallel-execution.js
@@ -4,5 +4,29 @@ import _ from 'lodash';
 import async from 'async';
 
 // BEGIN
-
+export const getDirectorySize = (directoryPath, callback) => {
+    let directorySize = 0;
+    fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+            callback(err);
+            return;
+        }
+        const directoryFiles = files.map((file) => path.join(directoryPath, file))
+        async.map(directoryFiles, fs.stat, (err, results) => {
+            if (err) {
+                callback(err);
+                return;
+            }
+            else {
+                directorySize = _.sumBy(results, (stat) =>
+                {
+                    if (stat.isFile())
+                        return stat.size;
+                    return 0;
+                });
+                callback(null, directorySize);
+            }
+        })
+    })
+}
 // END
