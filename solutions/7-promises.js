c8e2209466954cd503b9f8eb5d2a0893c90cca68
dima55279 <dima55279@yandex.ru>
2025-05-01 15:00:57 +0300

    Выполнено задание 7.

--- solutions/7-promises.js
+++ solutions/7-promises.js
@@ -1,5 +1,9 @@
 import fsp from 'fs/promises';
 
 // BEGIN
-
+export const reverse = (file) => {
+    return fsp.readFile(file, 'utf8')
+        .then((content) => content.split('\n').reverse().join('\n'))
+        .then((content) => fsp.writeFile(file, content))
+}
 // END
