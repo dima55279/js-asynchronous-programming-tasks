7637244cf6bef4580c359b26ccf749d90a64ee06
dima55279 <dima55279@yandex.ru>
2025-05-01 17:06:20 +0300

    Выполнено задание 9.

--- solutions/9-chain-of-promises.js
+++ solutions/9-chain-of-promises.js
@@ -1,5 +1,11 @@
 import fsp from 'fs/promises';
 
 // BEGIN
-
+export const getTypes = (filepathArray) => {
+    return Promise.all(filepathArray.map(path => fsp.stat(path)
+            .then(stat => stat.isDirectory() ? 'directory' : 'file')
+            .catch(() => null)
+        )
+    );
+}
 // END
