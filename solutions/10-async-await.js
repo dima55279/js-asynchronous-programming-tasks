35a21585501a086c3139dc11586827f0a4d59c73
dima55279 <dima55279@yandex.ru>
2025-05-01 17:08:59 +0300

    Выполнено задание 10. Закончена ЛР 6.

--- solutions/10-async-await.js
+++ solutions/10-async-await.js
@@ -1,5 +1,10 @@
 import fsp from 'fs/promises';
 
 // BEGIN
-
+export const exchange = async (file1, file2) => {
+    const data1 = await fsp.readFile(file1);
+    const data2 = await fsp.readFile(file2);
+    await fsp.writeFile(file1, data2);
+    await fsp.writeFile(file2, data1);
+}
 // END
