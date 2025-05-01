bb29b823cf313cb853f0d195615d6f8a79baa09a
dima55279 <dima55279@yandex.ru>
2025-05-01 15:37:43 +0300

    Выполнено задание 8.

--- solutions/8-promises-catch.js
+++ solutions/8-promises-catch.js
@@ -1,5 +1,10 @@
 import fsp from 'fs/promises';
 
 // BEGIN
-
+export const touch = (filepath) => {
+    return fsp.access(filepath, fsp.constants.R_OK | fsp.constants.W_OK)
+        .catch(() => {
+            fsp.writeFile(filepath, "")
+        })
+}
 // END
