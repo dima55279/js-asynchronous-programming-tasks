50b7b603a3f02d9c460486b18ba4601f5204560b
dima55279 <dima55279@yandex.ru>
2025-05-01 14:53:54 +0300

    Выполнено задание 6.

--- solutions/6-timers.js
+++ solutions/6-timers.js
@@ -1,5 +1,22 @@
 import fs from 'fs';
 
 // BEGIN
-
+const watch = (filepath, timer, callback) => {
+    const time = Date.now();
+    const id = setInterval(() => {
+        fs.stat(filepath, (error, stats) => {
+            if (error) {
+                clearInterval(id);
+                callback(error);
+                return;
+            }
+            let changeTime = stats.mtimeMs;
+            if (changeTime > time) {
+                callback(null);
+            }
+        })
+    }, timer)
+    return id;
+}
+export default watch;
 // END
